79e7bdb204605912f4d1607e5b0ce9e95cac78b8
abstudent <anna.bangerter@ucas-edu.net>
2026-01-09 09:45:03 -0700

    finicial calculator pseudocode

--- Individual_projects/finacial_calculator.py
+++ Individual_projects/finacial_calculator.py
@@ -1,16 +1,18 @@
 #AB 1st Finacial Calculator
 
+#Project analysis
 #Save Goal
 #inputs amount, amount of months
 #output amount per prefered consis
 #Actions:
 #amount divided by # of months
 
+
 #Compound Interest
 #inputs starting amount, interest percentage, time
 #output end amount
 #Actions:
-#starting amount multiplyed py interest
+#starting amount multiplyed by interest
 #do this for an amount of time
 
 #Budget
@@ -33,6 +35,37 @@
 #cost mulitiplied by percentage
 #add percentage to cost
 
-#Main
-#input action to complete
-#output answer from function
+#Pseudocode
+#FUNCTIONS
+#multiply function
+    #take two numbers get the product
+    #return the product
+
+#divide function
+    #take two numbers and get the quotient
+    #return the quotient
+
+#Save Goal function
+    #parameters are the amount you want to save(save) and the number of months(months)
+    #answer is call the multiply funtion and use the numbers as arguments
+    #return f"If you want to save {save} for {months}, you have to put in {answer} per month."
+
+#Compond Interest function
+    #start is the starting amount
+    #interest is the interest percentage
+    #time is the number of months the money will be left
+    #for i in time
+        #interest funtion
+            #start multiplied by interest
+            #return start
+    #return interest
+
+#Budget funtion
+    #parameters are a kwarg of all the catigories and the percentages  and you income
+    #for i in kwarg.key
+        #number is call multiply funtion and use the percentage and income
+        #i is number
+    #return kwarg
+
+
+
