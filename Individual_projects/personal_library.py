5c72450504234205a4992d524d351a41aa5f7ea3
abstudent <anna.bangerter@ucas-edu.net>
2026-01-12 09:52:57 -0700

    started personal library did list notes

---
+++ Individual_projects/personal_library.py
@@ -0,0 +1,22 @@
+#AB 1st Personal library
+#Project Analysis
+#Books
+#list
+# book name and the author
+
+#Adding new items
+#necessary info: new item name and author
+#actions:
+#get name
+#get author
+#format it to put in the list
+#put in list
+
+#search items
+
+#remove items
+#info: get the number of the item to be removed
+#actions:
+#remove item
+#tell them what it was
+
--- notes/lists.py
+++ notes/lists.py
@@ -1,2 +1,26 @@
-#AB 1st list
+#AB 1st list types notes
+#lists
+siblings = ["Anna", "Kailyn", "Lauren", "Danielle", "Jason"]
+
+print(siblings[3])
+siblings[-1] = "Dude"
+
+print(siblings)
+
+#tuples
+fruit = ("apple", "banana", "peach", "kiwi", "raspberry")
+home = (0,0)
+x,y = home
+
+print(x)
+
+#set
+colors = {"orange", "purple", "green", "blue", "yellow","red"}
+colors.add("pink")
+colors.remove("purple")
+for i in colors:
+    if i == "orange":
+        i = "marron"
+    print(i)
+print(colors)
 
