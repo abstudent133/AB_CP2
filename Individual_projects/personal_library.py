63e4c2f720dc6a83be97b2acfc1af2e1c5e29a15
abstudent <anna.bangerter@ucas-edu.net>
2026-01-15 09:51:34 -0700

    worked on pseudocode for personal library

--- Individual_projects/personal_library.py
+++ Individual_projects/personal_library.py
@@ -11,6 +11,7 @@
 #get author
 #format it to put in the list
 #put in list
+#confirm what they added
 
 #search items
 #necessary info: defining trait for search
@@ -22,6 +23,31 @@
 #remove items
 #info: get the number of the item to be removed
 #actions:
+#show all possible items
+#confirm it is the right item
 #remove item
-#tell them what it was
+
+#Pseudocode
+#books = list with mini tuples of the book title and author
+
+#add item function
+#parameters book info list
+#new_name = input new name
+#new_title = input new title
+#add a tuple of the name and title to books list
+#return books
+
+#remove items funtion
+#parameters book info list
+#show all the names of the books and there authors and have them numbered
+#choice = the number of the book they want to remove
+#remove the book by taking the number choice subtracting 1 and poping that index
+#return books list
+
+#search items function
+#intake books list
+#choice = input ask it they want to search by author or title
+#searching = ask what it is they are looking for
+#result = 0
+#
 
