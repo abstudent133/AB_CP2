ee2a6e722dc226642b2e35d88824349ac332103a
abstudent133 <anna.bangerter@ucas-edu.net>
2026-01-21 00:15:33 +0000

    fixed for rubric

--- Individual_projects/personal_library_extra.py
+++ Individual_projects/personal_library_extra.py
@@ -1,102 +1,23 @@
-#AB 1st Personal library
-#Project Analysis
-#Books
-#list
-# book name and the author
-
-#Adding new items
-#necessary info: new item name and author
-#actions:
-#get name
-#get author
-#format it to put in the list
-#put in list
-#confirm what they added
-
-#search items
-#necessary info: defining trait for search
-#actions:
-#have user choose which trait to use
-#search list based on that
-#tell user which books match the search
-
-#remove items
-#info: get the number of the item to be removed
-#actions:
-#show all possible items
-#confirm it is the right item
-#remove item
-
-#Pseudocode
-#books = main funtion
-
-#view function
-#parameters are the book list
-#result is an empty list
-#for each of the tuples in book list
-#format it 
-#add it to result
-#return result
-
-#add item function
-#parameters book info list
-#print instructions
-#new_name = input new name
-#first, last = new_name.split(" ")
-#print instruction
-#new_title = input new title
-#add a tuple of the names and title to books list
-#return books
-
-#remove items funtion
-#parameters book info list
-#show all the names of the books and there authors and have them numbered
-#choice = the number of the book they want to remove
-#remove the book by taking the number choice subtracting 1 and poping that index
-#return books list
-
-#search items function
-#intake books list
-#choice = input ask it they want to search by author or title
-#searching = ask what it is they are looking for
-#result = []
-#use nested for loops to check the information in by books list to see if it matches the search
-#for item in range(books):
-    #for info in range(books[item])
-        #if info == searching
-            #result += item
-        #else:
-            #continue
-#use conditionals to check if there is a result
-#if ther was a result use a for loop to formate the info in a new list
-#return that list
-
-#main function
-#books list
-#while loop
-#print all the options with a number
-#choice = choice of what to do
-#if choice is 1
-    #print(view function)
-#elif choice is 2
-    #result = run add function
-#elif choice is 3
-    #result = run remove function
-#elif choice is 4
-    #print search function
-#elif choice is 5
-    #print message about personal library
-    #break
-#else:
-    #print invalid choice
-    #continue
-#books = result
-#after while loop
-#return books
-
-
-
 #Code
+#save funtion
+def saveBooks(books):
+    file = open("library.txt", "w")
+    for book in books:
+        file.write(f"{book[0]}|{book[1]}|{book[2]}\n")
+    file.close()
+
+#load books funtion
+def loadBooks():
+    books = []
+    try:
+        file = open("library.txt", "r")
+        for line in file:
+            title, first, last = line.strip().split("|")
+            books.append([title, first, last])
+        file.close()
+    except FileNotFoundError:
+        books = []
+    return books
 #view function
 def view(books):
 #parameters are the book list
@@ -149,8 +70,8 @@ def remove(books):
     print("Please input the number of the item you would like to remove")
     choice = int(input("Number: "))
 #remove the book by taking the number choice subtracting 1 and poping that index
-    removed_books = books.pop(choice-1)
-    print(f"You removed {removed_books[0]} by {removed_books[1]} {removed_books[2]}")
+    remove = books.pop(choice-1)
+    print(f"You removed {remove[0]} by {remove[1]} {remove[2]}")
   
 #return books list
     return books
@@ -192,7 +113,8 @@ def search(books):
 
 #main function
 def main():
-    books = []
+#books list
+    books = load_books()
 #while loop
     while True:
         if books != []:
@@ -203,6 +125,7 @@ def main():
             "3. Remove\n" \
             "4. Search\n" \
             "5. Exit\n" \
+            "6. Delete library" \
                 )
             choice = int(input("Number: "))
         else:
@@ -233,8 +156,14 @@ def main():
             print(search(books))
 #elif choice is 5
         elif choice == 5:
-            print("Thank you for using your personal library.")
+            save_books(books)
+            print("Library saved. Thank you for using your personal library.")
             break
+#elif choice is 6:
+        elif choice == 6:
+            books = []
+            save_books(books)
+            print("Library reset.")
 #else:
         else:
     #print invalid choice
