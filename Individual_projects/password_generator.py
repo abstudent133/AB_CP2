5e5f1674a2e105004b90dd959680f8033d9581ef
abstudent <anna.bangerter@ucas-edu.net>
2026-01-23 09:52:06 -0700

    worked on psuedocode for password generator

---
+++ Individual_projects/password_generator.py
@@ -0,0 +1,33 @@
+#AB 1st Password Generator
+#Project Analysis
+#lower case
+#actions
+#generate a random number between 97 and 122
+#get that letter based on ascii
+
+#upper case
+#actions
+#generate a random number between 65 and 90
+#get the letter based on ascii
+
+#special character
+#actions
+#generate a random number between 33 and 47,58 and 64
+#get that symbol in ascii
+
+#number
+#actions
+#generate random number betweem 48 and 57
+#get that number based on ascii
+
+#Psuedocode
+#random function
+    #intake the range
+    #num is a random integer between the numbers of range
+    #character is chr(num)
+    #return character
+
+#generator
+    #length is ask the user for the length
+    #for range(length)
+        #
