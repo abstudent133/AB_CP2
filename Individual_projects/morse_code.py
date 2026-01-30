8618bd278738a03ee22ba2cf4acbcb06bd5c1a35
abstudent <anna.bangerter@ucas-edu.net>
2026-01-30 09:51:23 -0700

    started psuedocode for morse code translator

---
+++ Individual_projects/morse_code.py
@@ -0,0 +1,25 @@
+#AB 1st Morse Code Translator
+#Project Analysis
+#English to Morse
+#intake the phrase
+#action
+#for each of the letters change it to the morse 
+#show the user the new message
+
+#Morse to English
+#intake the morse
+#action
+#make sure it is morse
+#for each of the morse symbols translate it to english
+#format it for proper grammar
+#show user
+
+#Pseudocode
+#alphabet = list of all the letters in the alphabet(lowercase)
+#morse = list of all the letters in morse code corrisponding to the alphabet list
+
+#morse to english function
+    #intake alphabet list and morse list
+    #print key function
+    #ask for there message in morse code (note: tell them to add an / between words and a space between each letter)
+    #
