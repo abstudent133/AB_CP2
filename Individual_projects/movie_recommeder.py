9b5f16f3b87f393cc64bb4a534bd0c7e85eb7047
abstudent <anna.bangerter@ucas-edu.net>
2026-02-03 09:52:42 -0700

    started movie recommender

---
+++ Individual_projects/movie_recommeder.py
@@ -0,0 +1,18 @@
+#AB 1st Move Recommender
+#Project Analysis
+#access
+#action
+#get csv file
+#convert each line into a dictionary
+#put each dictionary in a list
+
+#filters
+#actions
+#ask for which filters they want
+#formate it into a list
+#ask about what they are searching for for each filter chosen
+#return that list
+
+#genre search
+#intake the requesed genre and the list of dictionaries
+#
--- notes/reading_files.py
+++ notes/reading_files.py
@@ -1,4 +1,5 @@
 #AB 1st Reading Files Notes
+import csv
 while True:
     try:
         with open("notes/reading.txt","r") as file:
@@ -13,13 +14,13 @@ while True:
 
 
 try:
-    with open("notes\spreadsheet.csv",mode="r") as csv_file:
+    with open("notes\Class CSV sample - Sheet1.csv",mode ="r") as csv_file:
         content = csv.reader(csv_file)
         header = next(content)
         rows = {}
-        rows.append((header[0]: line[0], header[1]: line[1]))
-        for line in csv_file:
-            print(f"{line[0]}: {line[1]}")
+        rows.update({header[0]: line[0], header[1]: line[1]})
+        for line in content:
+            print(f"{line[0]} favorite color is {line[1]}")
 except:
     print("this code doesn't work")
 else:
