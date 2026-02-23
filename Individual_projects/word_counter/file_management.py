46ba222d48ee0efa03bc86f57829e7b9457a051e
abstudent133 <anna.bangerter@ucas-edu.net>
2026-02-23 09:50:11 -0700

    file manamgement

--- Individual_projects/word_counter/file_management.py
+++ Individual_projects/word_counter/file_management.py
@@ -95,4 +95,88 @@
     #ask user for the relative path
     #return relative path
 
+#Code
+#import time management
+import time_management
 
+#open_file function
+def open_file(action):
+#parameters: action
+    #relative_path = call relative_path_func()
+    relative_path = relative_path_func()
+    #if action is view
+    if action == "view":
+        #try to open the file with relative path and read as file:
+        try:
+            string = """"""
+            with open(relative_path, "r") as file:
+            #read each line and add it to the total string
+                for line in file:
+                    string += line
+            #split the huge string at the first enter
+            parts = string.split("" \
+            "")
+            #display the first part that is the part they've writen
+            print(parts[0])
+        #except if that doesn't work
+        except:
+            #open a file with the relative path and read and write as file
+            with open(relative_path, "w") as file:
+            #display a message about an empty document
+                print("This is an empty document")
+    #if action is append
+    elif action == "append":
+        #ask user what they want to add
+        add_this = input("What would like to add to your document(press enter when you are done): ")
+        #open read each line and add to the total string
+        with open(relative_path, "r+") as file:
+            string = file.read()
+        #split at the first enter
+            parts = string.split("" \
+            "")
+        #append the new stuff to the first part of the split
+            parts[0].append(add_this)
+            for part in parts:
+                string_two += part
+        #write the new stuff into the file
+            file.write(string_two)
+
+
+#update info function
+def update_info(relative_path):
+#parameters: relative path
+    #create the variable time as the time function
+    time = time_func()
+    #word_count is word_count_func
+    word_count = word_count_func(relative_path)
+    #formate time and word_count into a multi-line string
+    formate = f"""Words: {word_count}
+    Last Updated: {time}"""
+    #try to open the file with relative path and read+ as file
+    try:
+        #read each line and turn it into a string
+        #add to the bottom of that string
+        #write over the current file with this one
+        #close the file
+    #except open a file with relative path and write as file
+        #write a new file
+        #write the formated word_count and time string
+        #close file
+    #print that the file was updated and show word count
+
+#word_counter_func
+#parameters:relative_path
+    #try opening file with relative path as file
+        #read the file and turn it into a string
+        #split the string at the first enter
+        #words is the string the user wrote.split
+        #count is len(string)
+    #except:
+        #count is 0
+    #return count
+
+#relative_path_func
+#parameters:none
+    #tell user what a relative path is 
+    #ask user for the relative path
+    #return relative path
