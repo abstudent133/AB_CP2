9a74543226fbb4125b306a84e8a630932061eab6
abstudent133 <anna.bangerter@ucas-edu.net>
2026-02-27 09:52:43 -0700

    started fractal pattern

---
+++ Individual_projects/fractal_pattern_generator.py
@@ -0,0 +1,24 @@
+#AB 1st Fractal Pattern Generator
+#Project Analysis
+
+#side length change function
+#parameters:side length
+#actions:
+#divide side length by 2
+#return the new side length
+
+#draw funciton
+#parameters: depth
+#actions:
+#start at the starting point
+#get a starter side length based on depth
+#draw a triangle
+#draw the line for one of the next smallest
+#repeat that for the depth
+#start filling in the next biggest triangle
+#repeat that for the whole triangle
+#start with the bottom right corner
+
+#find position function
+#parameters: first 
+
