f37893f67985eecd8d6d2d3b2e9d84c210bd661d
abstudent133 <anna.bangerter@ucas-edu.net>
2026-03-18 09:38:34 -0600

    set up files for geometric calculator

---
+++ Individual_projects/geomentric_calculator/equation_managment.py
@@ -0,0 +1,3 @@
+#This is Equation Management and it includes:
+#Viewing all the equations used when finding the measurments of shapes
+#Taking an input from the user and getting the rest of the values
---
+++ Individual_projects/geomentric_calculator/shape_management.py
@@ -0,0 +1,5 @@
+#This is Shape Management including:
+#Creating New Shapes
+#Viewing all shapes
+#Compare Shapes
+#Sort Shapes
