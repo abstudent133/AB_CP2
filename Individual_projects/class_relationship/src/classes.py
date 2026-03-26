cd9b6d7579167483a3fb7865e0248d809c1f7a45
abstudent133 <anna.bangerter@ucas-edu.net>
2026-03-26 09:53:07 -0600

    started class relationship project

---
+++ Individual_projects/class_relationship/src/classes.py
@@ -0,0 +1,56 @@
+#Classes File
+#This file will hold all the classes
+
+#Planning and Analysis
+
+#Student class
+#parameters: student_id, student_name, list of grades
+#initiate
+#string all the info together in a pretty way
+#add grades
+#intake a grade then add it to the list
+#calculate average
+#take all the scores add them together then divide by the length
+#calculate grade
+#use conditional to take the average of the grades and find the letter grade it belongs to
+
+#GradeBook class
+#parameters: none
+#initiate
+#add student
+#add student with student class
+#save that student to the csv
+#find student
+#show all the students
+#ask them to input which student they want with the id
+#show them the information by calling the string method in the student class
+#show class info
+#get all the average grades
+#put them into a list
+#average them
+#get the highest score
+#get the lowest score
+#total number of students
+
+#Pseudocode
+
+#student class
+#parameters: student_id, student_name, list of grades
+    #initiate all things with self
+    #string
+        #take all the information including the info found using other methods and formate it in a pretty way
+    #add grades
+        #ask for new grade
+        #make sure it is a valid grade input
+        #add it to the list of grades
+        #create a formated print that shows how many grades and the new average
+        #return that list of grades
+    #average
+        #intake the list of grades
+        #add them all together
+        #divide the length of the list
+    #letter grade
+        #intake the average
+        #letter_grade_list
+        #use conditionals based on letter grade list to get the correct letter grade
+
