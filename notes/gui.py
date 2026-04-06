6e8bbd6f81d6aadc34ac80666d2a92fd7b783d9a
abstudent133 <anna.bangerter@ucas-edu.net>
2026-04-06 09:51:37 -0600

    did gui notes

---
+++ notes/gui.py
@@ -0,0 +1,26 @@
+#GUI with Tkinter Notes
+import tkinter as tk
+
+
+root = tk.Tk()
+
+root.title("Testing")
+root.configure(background="pink")
+root.minsize(250, 250)
+root.maxsize(1000, 1000)
+root.geometry("300x300+100+100")
+label = tk.Label(root, text="This is currently working.", font=("Times New Roman",14,"bold"))
+label.config(fg="magenta", background="pink")
+#stuff about button
+root.count = 0
+def add():
+    root.count += 1
+    tk.Label(root, text=root.count).pack()
+
+btn = tk.Button(root, text="ADD",command=add)
+btn.pack()
+label.pack()
+#image = tk.PhotoImage(file="img\\letter.png")
+#tk.Label(root, image=image).pack()
+
+root.mainloop()
